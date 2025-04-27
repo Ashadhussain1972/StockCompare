ac137397c74d8a5189ff08f46450dfe2bb05b2c3
Ashad_Hussain <123232550+Ashadhussain1972@users.noreply.github.com>
2025-04-27 18:44:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,28 +3,23 @@ from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app) 
 
-# Proxy settings (currently not used)
+
 proxies = {
     'http': 'http://lum-customer-hl_c9687491-zone-static:rcmzw7zjkz7m@zproxy.lum-superproxy.io:22225',
     'https': 'http://lum-customer-hl_c9687491-zone-static:rcmzw7zjkz7m@zproxy.lum-superproxy.io:22225'
 }
-
-proxies = " "
+proxies = ''
 
 def get_stocks_fundamental_data(stocks):
     all_data = []
     
     for stock in stocks:
         url = f'https://www.screener.in/company/{stock}/consolidated/'
-        try:
-            response = requests.get(url, proxies=proxies, timeout=10)
-        except requests.exceptions.RequestException:
-            continue
+        response = requests.get(url, proxies=proxies)
         
         if response.status_code != 200:
             continue
@@ -33,48 +28,39 @@ def get_stocks_fundamental_data(stocks):
         data = {"stock": stock}
         
         # Get fundamental data
-        try:
-            ratio_container = soup.find("div", {"class": "company-ratios"}).find("ul").findAll("li")
-            for ratio in ratio_container:
-                name = ratio.find("span", {"class": "name"}).text.strip()
-                value = ratio.find("span", {"class": "number"}).text.strip()
-                data[name] = value
-        except:
-            pass
+        ratio_container = soup.find("div", {"class": "company-ratios"}).find("ul").findAll("li")
+        for ratio in ratio_container:
+            name = ratio.find("span", {"class": "name"}).text.strip()
+            value = ratio.find("span", {"class": "number"}).text.strip()
+            data[name] = value
         
         # Get quarterly data
-        try:
-            qtrly_container = soup.findAll("div", {"class": "responsive-holder fill-card-width"})[0]
-            header = qtrly_container.find("thead").findAll("th")[-4:]
-            header = [head.text.strip() for head in header]
-            rows = qtrly_container.find("tbody").findAll("tr")
-            for row in rows[:-1]:
-                cols = row.findAll("td")[-4:]
-                row_name = row.find("td").text.strip().replace("\xa0+", "")
-                for col, head in zip(cols, header):
-                    name = f"{row_name} {head}"
-                    value = col.text.strip()
-                    data[name] = value
-        except:
-            pass
+        qtrly_container = soup.findAll("div", {"class": "responsive-holder fill-card-width"})[0]
+        header = qtrly_container.find("thead").findAll("th")[-4:]
+        header = [head.text.strip() for head in header]
+        rows = qtrly_container.find("tbody").findAll("tr")
+        for row in rows[:-1]:
+            cols = row.findAll("td")[-4:]
+            row_name = row.find("td").text.strip().replace("\xa0+", "")
+            for col, head in zip(cols, header):
+                name = f"{row_name} {head}"
+                value = col.text.strip()
+                data[name] = value
         
         # Get balance sheet data
-        try:
-            bal_container = soup.find("section", {"id": "balance-sheet"}).find("div", {"class": "responsive-holder fill-card-width"})
-            header = bal_container.find("thead").findAll("th")[-4:]
-            header = [head.text.strip() for head in header]
-            rows = bal_container.find("tbody").findAll("tr")
-            for row in rows:
-                cols = row.findAll("td")[-4:]
-                row_name = row.find("td").text.strip().replace("\xa0+", "")
-                if "Raw PDF" in row_name:
-                    continue
-                for col, head in zip(cols, header):
-                    name = f"{row_name} {head}"
-                    value = col.text.strip()
-                    data[name] = value
-        except:
-            pass
+        bal_container = soup.find("section", {"id": "balance-sheet"}).find("div", {"class": "responsive-holder fill-card-width"})
+        header = bal_container.find("thead").findAll("th")[-4:]
+        header = [head.text.strip() for head in header]
+        rows = bal_container.find("tbody").findAll("tr")
+        for row in rows:
+            cols = row.findAll("td")[-4:]
+            row_name = row.find("td").text.strip().replace("\xa0+", "")
+            if "Raw PDF" in row_name:
+                continue
+            for col, head in zip(cols, header):
+                name = f"{row_name} {head}"
+                value = col.text.strip()
+                data[name] = value
         
         all_data.append(data)
     
@@ -92,5 +78,4 @@ def get_stocks_data():
     return jsonify(data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable if available
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
